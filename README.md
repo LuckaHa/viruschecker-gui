012c250b185037ea578e34498578996fa767b6a8
unknown <5187016@upjs.sk>
2019-12-27 15:08:43 +0100

    search report by regex

--- README.md
+++ README.md
@@ -2,31 +2,31 @@
 
 GUI for Virus Checker using Vue.js
 
-First, you need to run Virus Checker
+I. First, you need to run Virus Checker:
 
-1. Ak boli zmeny v kóde, kompilovať projekt - do compilera v projekte:
+1. Compile project in project compiler (after every change in project):
 	gradlew.bat clean shadowJar
 
 2. DRIVER
-Spustiť VM. Na mieste, kde je driver (driver-0.23.0-all.jar) otvoriť cmd a zadať:
+Load VM. In the location of driver (driver-0.23.0-all.jar) open cmd and write:
 	java -jar driver-0.23.0-all.jar -a
-Teraz beží driver na VM na  porte 8080. 
-To isté by malo byť na hostovi na porte 8080. 
-Zobrazí sa tam json: {"driverVersion":..., "antiviruses:"...}
+Now driver runs on VM on port 8080. 
+The same should run on host on port 8080. 
+You will see the json: {"driverVersion":..., "antiviruses:"...}
 
 3. GATEWAY
-Ak bol kód prekompilovaný treba znova vytvoriť súbor 'driverUrls.txt'.
-Na hoste v tom istom priečinku ako je 'driverUrls.txt' spustiť:
+If the code was recompiled, you need to create file 'driverUrls.txt' again.
+On host in the location of file 'driverUrls.txt' run:
 	java -jar gateway-0.23.2-all.jar driverUrls.txt
-Gateway beží na http://127.0.0.1:8080/
-GET /driversInfo potom zavoláme takto http://127.0.0.1:8080/driversInfo
+Gateway runs at http://127.0.0.1:8080/
+Then you can call GET /driversInfo potom like this: http://127.0.0.1:8080/driversInfo
 
 4. CLIENT/WEB APP
-V priečinku viruschecker/client-web/build/libs/ otvoriť cmd a spustiť:
+In the folder viruschecker/client-web/build/libs/ open cmd and run:
 	java -jar client-web-0.23.2-all.jar http://localhost:8080
 	192.168.1.1	
 
-Then start this GUI
+II. Then start this GUI:
 1. Install:
 	npm install --save axios vue-axios
 	
--- src/components/SearchReports.vue
+++ src/components/SearchReports.vue
@@ -3,7 +3,37 @@
     <v-layout wrap>
       <v-flex xs4>
           <div class="container">
-          <p>{{port}} <br> {{wholeResponse}}</p>
+            <div v-for="scan in wholeResponse.data" :key="scan">
+              <h1>Scan report for {{scan.report.filename}}</h1>
+                <!-- <p>Time: <span>{{ wholeResponse.data.report.date | moment("YYYY-MM-DD, hh:mm:ss") }}</span></p> -->
+                <!-- <span>{{ wholeResponse.data.report.date | formatDate}}</span> -->
+                <p>Time: {{myDate}}</p>
+                <h2>Scan result</h2>
+                <div v-if="scan.report.status === 'OK'">
+                  <p class="green--text">{{scan.report.status}}</p><br>
+                </div>
+                <div v-else>
+                  <p class="red--text">{{scan.report.status}}</p><br>
+                </div>
+                <p>SHA-256 <br> {{scan.sha256}}</p>
+                <p>SHA-1 <br> {{scan.sha1}}</p>
+                <p>MD5 <br> {{scan.md5}}</p><br><br>
+                <h3>Antivirus reports</h3>
+                <v-divider></v-divider>
+                <div v-for="result in scan.report.results" :key="result">
+                  <div v-if="result.malwareDescription === 'OK'">
+                    <div class="green--text">{{result.malwareDescription}}</div> {{result.antivirus}}  {{result.virusDatabaseVersion}}
+                  </div>
+                  <div v-else>
+                    <div class="red--text">{{result.malwareDescription}}</div> {{result.antivirus}}  {{result.virusDatabaseVersion}}
+                  </div>
+                  <v-divider style="width: 20px;"></v-divider>
+                </div>
+                <div v-if="checked">
+                  <p class="red--text">TODO: VirusTotal</p><br>
+                </div>
+                <br>
+            </div>
           </div>
       </v-flex>
     </v-layout>
@@ -52,13 +82,18 @@ export default {
     // Submits message to the server
     submit () {
           this.loading = true;
-          axios.get( '/scanReportBy/${message}')
-          //axios.get( '/scanReportBy/'.concat(message))
+          // upravit hladany vyraz tak, aby boli scany oddelene ciarkami
+          this.message = this.message.replace(/, /g, ",");
+          this.message = this.message.replace(/ /g, ",");
+          if(!this.message.endsWith(',')) {//charAt(this.message.length-1).en) {
+            this.message = this.message.concat(',');
+          }
+          // najst vysledky
+          axios.get( '/scanReportBy/'.concat(this.message))
           .then(response => {
       this.wholeResponse = response,
       this.loading = false,
       this.loaded = true,
-      console.log('/scanReportBy/${message}'),
       console.log('SUCCESS')
       })
       .catch(function(){
